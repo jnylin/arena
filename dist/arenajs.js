5073baf66d118767dae8e09c736cdd305003f5cb
Jakob Nylin <jakob.nylin@gmail.com>
2014-11-30 08:42:18 +0100

    Dvd.prototype.cover

--- dist/arenajs.js
+++ dist/arenajs.js
@@ -1,4 +1,4 @@
-/*! arenajs - v0.1.0 - 2014-11-29
+/*! arenajs - v0.1.0 - 2014-11-30
 * https://github.com/jnylin/arena
 * Copyright (c) 2014 Jakob Nylin; Licensed GPL */
 
@@ -48,7 +48,7 @@ function CatalogueRecord(e, view) {
 	author = this.subElements.author.text();	
 	publisher = this.subElements.publisher.text();
 	year = this.subElements.year.text();
-	media = this.subElements.media.text();
+	media = this.subElements.media.text().trim();
 	lang = this.subElements.lang.text().trim();
 	
     /*	ISBN är olika uppmärkt i träfflistan och katalogpostsidan
@@ -138,6 +138,10 @@ CatalogueRecord.prototype.truncateTitle = function() {
 };
 
 // Mervärden
+CatalogueRecord.prototype.dvd = function() {
+	new Dvd(this);
+};
+
 CatalogueRecord.prototype.ljudprov = function() {
 	new Ljudprov(this);
 };
@@ -232,6 +236,28 @@ DetailViewMethods.prototype.addLnkToExtRes = function(url, lnkTxt, lnkTitle, tar
 	$('#youtube').show();
 }*/
 
+function Dvd(record) {
+	this.record = record;
+
+    var tmdb = new Tmdb('de9f79bfc08b502862e4d8bba5723414', this),
+		query;
+
+	query = record.title.main;
+
+	if ( record.title.sub ) {
+		query += ' ' + record.title.sub;
+	}
+		
+	tmdb.search(query);
+
+	console.log(tmdb);
+	console.log(this);
+}
+
+Dvd.prototype.cover = function(tmdb) {
+	this.record.subElements.bookJacket.find('img').attr('src', tmdb.urlPoster);
+	this.record.subElements.cover.append('<a href="' + tmdb.url + '/' + tmdb.mediaType + '/' + tmdb.id + '?language=sv' + '" target="_blank" title="Information hos TMDb">themoviedb.org</a>');
+};
 
 function ListViewMethods(record) {
 	try {
@@ -426,7 +452,183 @@ function Title(str,origTi) {
 	}
 }
 
+function Tmdb(apiKey, dvd) {
+	this.apiKey = apiKey;
+	this.dvd = dvd;
+}
 
+Tmdb.prototype.url = 'http://www.themoviedb.org';
+// Egenskaper för API:t
+Tmdb.prototype.api = 'http://api.themoviedb.org/3/';
+Tmdb.prototype.pathLogo = "/documents/58068/140667/tmdb.png/fa3903c1-b170-4ab2-970a-baf9264001d9?t=1387215209438";
+Tmdb.prototype.baseUrlImg = 'http://image.tmdb.org/t/p/w';
+Tmdb.prototype.posterSizes = [92, 154, 185, 342, 500, 780];
+
+// API-funktioner
+Tmdb.prototype.search = function(query) {
+	console.log(this);
+	console.log("query = " + query);
+
+	$.ajax({
+		type: 'GET',
+		url: this.api + 'search/multi?api_key=' + this.apiKey + '&query=' + encodeURIComponent(query) + '&language=sv',
+		datatype: 'jsonp',
+		success: this.searchCallback(this)
+	});
+
+};
+
+Tmdb.prototype.searchCallback = function(thisObj) {
+	return function(json) {
+		var arrResults = json.results,
+			arrCandidates = [],
+			movie, tv,
+			diffYear;
+
+		/* HANTERA TRÄFFLISTAN */
+		// Jämför svensk/tillgänglig titel
+		for (var i=0;i<arrResults.length;i++) {
+			if ( arrResults[i].media_type === "movie" && thisObj.movieIsInListOfHits(arrResults[i], "ti") === true ) {
+				movie = arrResults[i];
+				diffYear = thisObj.dvd.record.year - REG_EXP_YEAR.exec(movie.release_date);	
+				arrCandidates.push([diffYear,i]);					
+			}
+			else if ( arrResults[i].media_type === "tv" && thisObj.tvIsInListOfHits(arrResults[i]) ) {
+				tv = arrResults[i];
+			}
+		}
+
+		// I värsta fall liknande titel
+		if ( arrCandidates.length < 1 ) {
+			for (i=0;i<arrResults.length;i++) {
+				if ( arrResults[i].media_type === "movie" && thisObj.movieIsProbablyInListOfHits(arrResults[i]) === true ) {
+					movie = arrResults[i];
+					diffYear = thisObj.dvd.record.year - REG_EXP_YEAR.exec(movie.release_date);
+					arrCandidates.push([diffYear,i]);						
+				}
+			}
+		}
+
+		// Välj bästa kandidaten
+		if ( arrCandidates.length < 1 ) {
+			movie = "";
+		}
+		else {
+			// Sortera på skillnad i utgivningsår (diffYear)
+			arrCandidates.sort(function(a, b) { return ( (a[0] < b[0]) ? -1 : ( (a[0] > b[0]) ? 1 : 0 ) ); });
+			movie = arrResults[arrCandidates[0][1]];
+		}
+
+		// Sätt egenskaper
+		if ( movie !== '' ) {
+			thisObj.id = movie.id;
+			thisObj.mediaType = 'movie';
+			thisObj.setUrlPoster(movie.poster_path);
+		}
+		else if ( tv ) {
+			thisObj.id = tv.id;
+			thisObj.mediaType = 'tv';
+			thisObj.setUrlPoster(tv.poster_path);
+		}
+
+		// Töm resultatarrayen för att kunna göra en ny sökning
+		arrResults.length = 0;
+		
+		// Sätt omslag med mera
+		thisObj.dvd.cover(thisObj);
+	};
+
+};
+
+Tmdb.prototype.tv = function(id) {
+	$.ajax({
+		type: 'GET',
+		url: this.api + 'tv/' + id + '?api_key=' + this.apiKey + '&language=sv',
+		datatype: 'jsonp',
+		success: this.tvCallback(this, this.dvd.record.title.part)
+	});
+};
+
+Tmdb.prototype.tvCallback = function(thisObj, seasonNr) {
+	return function(json) {
+		var tvSeason;
+			
+		/* Exemplet Downton Abbey har en "special"-DVD */
+		if ( json.seasons.length === json.number_of_seasons ) {
+			seasonNr = seasonNr - 1;
+		}		
+		tvSeason = json.seasons[seasonNr];
+			
+		thisObj.posterPath = tvSeason.poster_path;
+	};
+};
+
+// Avgör om en titel finns i träfflistan
+Tmdb.prototype.movieIsInListOfHits = function(movie) {
+		var hit = false,
+			sameTitle = ( ( this.titleToCompare(this.dvd.record.title.main) === this.titleToCompare(movie.title) ) || ( this.titleToCompare(this.dvd.record.title.main) === this.titleToCompare(movie.original_title) ) );
+	
+		if ( sameTitle ) {
+			hit = true;
+		}
+	
+		return hit;				
+};
+
+Tmdb.prototype.movieIsProbablyInListOfHits = function(movie) {
+		var hit = false,
+			titleFromTmdb = this.titleToCompare(movie.title);
+		
+		if ( ( titleFromTmdb.search( this.titleToCompare(this.dvd.record.title.main) ) > -1 && titleFromTmdb.search( this.dvd.record.title.part ) > -1 ) || ( this.titleToCompare(this.dvd.record.title.main).search(titleFromTmdb) > - 1 ) ) {
+			hit = true;
+		}
+	
+		return hit;				
+};
+
+Tmdb.prototype.tvIsInListOfHits = function(tv) {
+		var hit = false,
+			sameTitle = ( this.titleToCompare(this.dvd.record.title.main) === this.titleToCompare(tv.name) );
+		
+		if ( sameTitle) {
+			hit = true;
+		}
+		
+		return hit;
+};
+
+Tmdb.prototype.titleToCompare = function(str) {
+	var pattResChar, title;
+	pattResChar = new RegExp(/[&+,\/:;=?@"-]/g);
+	title = str.toLowerCase();
+	title = title.replace(pattResChar,"");
+	title = title.replace(/ /g,"");
+	
+	return title;
+};
+
+// GET/SET-metoder
+Tmdb.prototype.getYoutubeId = function(json, id, lang) {
+
+	var youtubeId = '';
+
+	if ( json.youtube.length >= 1 ) {
+		youtubeId = json.youtube[0].source;
+	}
+
+	return youtubeId;
+};
+
+Tmdb.prototype.setUrlPoster = function(poster_path) {
+	if ( poster_path ) {
+		this.urlPoster = this.baseUrlImg + this.posterSizes[0] + poster_path;
+	}
+};
+
+// Globala
+window.REG_EXP_YEAR = new RegExp("[0-9]{4}", "i");
+
+/**********************/
 /* Diverse funktioner */
 /**********************/
 
