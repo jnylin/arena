1cd58adc099481627a594d9931fea1e0d44bdb27
Jakob Nylin <jakob.nylin@gmail.com>
2014-12-03 22:38:41 +0100

    Used the cachedScript function on the script for dynamic lists

--- liferay/dynamicList.js
+++ liferay/dynamicList.js
@@ -1,16 +1,10 @@
 (function($) {
-  $.ajax({
-    type: 'GET',
-    url: "http://bibliotek.vimmerby.se/documents/58068/138011/arenajs.min.js/ce0740d4-a718-4217-aa67-55035d95f6eb",
-    datatype: 'script',
-    cache: true,
-    success: function() {
+	$.cachedScript("http://bibliotek.vimmerby.se/documents/58068/138011/arenajs.min.js/ce0740d4-a718-4217-aa67-55035d95f6eb").done(function() {
 		var cssClass = 'portlet-listRecordSearchResult';
 
 		new SearchResult($('.'+cssClass));
 		Wicket.Ajax.registerPostCallHandler(function() {
 			new SearchResult($('.'+cssClass));
 		});
-	}
-  });
+	});
 }(jQuery));
