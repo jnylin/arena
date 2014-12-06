35ae2b51056a3f0575110af909b7bc86dc55cf7d
Jakob Nylin <jakob.nylin@gmail.com>
2014-12-06 16:12:37 +0100

    Some code to past in Liferay

---
+++ liferay/audioBooksList.js
@@ -0,0 +1,26 @@
+(function($) {
+	function init() {
+		$.cachedScript("http://bibliotek.vimmerby.se/documents/58068/138011/arenajs.min.js/ce0740d4-a718-4217-aa67-55035d95f6eb").done(function() {
+			var cssClass = 'portlet-listRecordSearchResult',
+					settings = {
+						hideFields: ['media']
+					};
+		
+				new SearchResult($('.'+cssClass), settings);
+				Wicket.Ajax.registerPostCallHandler(function() {
+					new SearchResult($('.'+cssClass), settings);
+				});
+		});
+	}
+
+	// Cross-domain AJAX för IE 8 och 9
+	if ( $('html').attr('class').match(/\b(ie9|ie8)\b/) ) {
+		$.cachedScript('http://cdnjs.cloudflare.com/ajax/libs/jquery-ajaxtransport-xdomainrequest/1.0.3/jquery.xdomainrequest.min.js').done( function() {
+			init();
+		});
+	}
+	else {
+		init();
+	}
+
+}(jQuery));
