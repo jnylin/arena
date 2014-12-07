1e572302c3fba1314bec814c802a1cf2053c45b9
Jakob Nylin <jakob.nylin@gmail.com>
2014-12-07 10:41:48 +0100

    Moved a paranthesis to the right place

--- liferay/dynamicList_truncate.js
+++ liferay/dynamicList_truncate.js
@@ -1,7 +1,6 @@
 (function($) {
 	function init() {
 		$.cachedScript("http://bibliotek.vimmerby.se/documents/58068/138011/arenajs.min.js/ce0740d4-a718-4217-aa67-55035d95f6eb").done(function() {
-			console.log("init started");
 			var cssClass = 'portlet-listRecordSearchResult',
 				settings = {
 					truncate: true
