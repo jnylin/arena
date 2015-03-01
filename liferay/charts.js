1ae16848d46a9667ae08f725b2bb5bb5133b0f40
Jakob Nylin <jakob.nylin@gmail.com>
2015-03-01 10:35:18 +0100

    Don't truncate titles on charts

--- liferay/charts.js
+++ liferay/charts.js
@@ -3,7 +3,6 @@
 		$.cachedScript("http://bibliotek.vimmerby.se/documents/58068/138011/arenajs.min.js/ce0740d4-a718-4217-aa67-55035d95f6eb").done(function() {
 			var cssClass = 'portlet-listRecordSearchResult',
 				settings = {
-					truncate: true,
 					removeParentheses: true,
 					hideFields: ['media']
 				};
